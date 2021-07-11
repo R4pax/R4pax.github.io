d20f1184f8a7cbf18d7d800e0ca0750b07c77e95
Rapaxalar <72609217+Rapaxalar@users.noreply.github.com>
2021-07-11 17:19:30 +0200

    proc p1-1

--- projects/javascript_full/p1/js/script.js
+++ projects/javascript_full/p1/js/script.js
@@ -6,25 +6,55 @@ const movieDB = {
     "Лига справедливости",
     "Ла-ла лэнд",
     "Одержимость",
-    "Скотт Пилигрим против всех",
-    "Шрек",
+    "Скотт Пилигрим против...",
   ],
 };
 
-document.querySelector(".promo__adv").style.display = "none";
+document.querySelector(".promo__adv").remove();
 document.querySelector(".promo__content").style.width = "calc(100% - 300px)";
 document.querySelector(".promo__genre").textContent = "Драма";
 document.querySelector(".promo__bg").style.backgroundImage = "url(img/bg.jpg)";
+const form = document.querySelector("form.add");
+const deleteBtns = document.querySelectorAll("div.delete");
 const moviesList = document.querySelector(".promo__interactive-list");
-moviesList.innerHTML = "";
+
+form.addEventListener("submit", addMovie);
+
+function addMovie(e) {
+  e.preventDefault();
+  const value = document.querySelector("input.adding__input").value;
+  const checkbox = document.querySelector("input[type='checkbox']").checked;
+  checkbox && console.log("Добавляем любимый");
+  movieDB.movies.push(value.length < 21 ? value : value.slice(0, 21) + "...");
+  refreshMoviesList();
+}
 
 const createListElem = (index, name) => {
   let li = document.createElement("li");
+  li.setAttribute("data-num", index);
   li.classList.add("promo__interactive-item");
   li.innerHTML = `${index} - ${name}<div class="delete"></div>`;
   return li;
 };
 
-movieDB.movies.sort().forEach((name, index) => {
-  moviesList.append(createListElem(index, name));
-});
+const refreshMoviesList = () => {
+  moviesList.innerHTML = "";
+  movieDB.movies.sort().forEach((name, index) => {
+    moviesList.append(createListElem(index, name));
+  });
+  addDeleteEvents();
+};
+
+const addDeleteEvents = () => {
+  document.querySelectorAll("div.delete").forEach((el) => {
+    el.addEventListener("click", deleteMovie);
+  });
+};
+
+function deleteMovie(e) {
+  const id = e.currentTarget.parentNode.dataset.num;
+  movieDB.movies.splice(id, 1);
+  refreshMoviesList();
+}
+
+refreshMoviesList();
