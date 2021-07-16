dd9dbdc3e319fc8d6b46a073008be437baa405f2
Roman Razhnyi <rapax@alarstudios.com>
2021-07-16 14:11:16 +0200

    Update blocks rotation

--- css-animations/block-rotation.js
+++ css-animations/block-rotation.js
@@ -1,8 +1,17 @@
+/*
+  добавить режим alternate чекбоксом
+  добавить параметр плотность блоков (в формулу расчёта задержки)
+  добавить стартовую задержку
+  ? продлить анимацию на еще одну итерацию
+*/
 const FLOOR = document.querySelector(".floor");
 const vars = {
   width: 128,
   height: 64,
-  radius: 25,
+  radius_1: 25,
+  radius_2: 25,
+  radius_3: 25,
+  radius_4: 25,
   count: 3,
   duration: 20,
 };
@@ -10,14 +19,17 @@ const vars = {
 function updateCssVars() {
   const opacity = (0.8 - vars.count / 180).toFixed(2);
   document.documentElement.style.setProperty("--o", opacity);
-  document.documentElement.style.setProperty("--br", vars.radius);
+  document.documentElement.style.setProperty("--r1-pc", vars.radius_1);
+  document.documentElement.style.setProperty("--r2-pc", vars.radius_2);
+  document.documentElement.style.setProperty("--r3-pc", vars.radius_3);
+  document.documentElement.style.setProperty("--r4-pc", vars.radius_4);
   document.documentElement.style.setProperty("--bw", vars.width + "px");
   document.documentElement.style.setProperty("--bh", vars.height + "px");
   document.documentElement.style.setProperty(`--a-dur`, vars.duration + "s");
 }
 
 function createElement(i) {
-  const delay = -((i * vars.duration) / vars.count).toFixed(2) + "s";
+  const delay = -((i * 0.5 * vars.duration) / vars.count).toFixed(2) + "s";
   const div = document.createElement("div");
   div.classList.add("block");
   div.style.setProperty(`--a-del`, delay);
